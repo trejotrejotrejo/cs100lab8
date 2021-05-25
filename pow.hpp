3d2adfe152d52360f1977e087273bf35ecf6bb94
rohanbehera03 <65427904+rohanbehera03@users.noreply.github.com>
2021-05-24 20:48:47 -0700

    Create pow.hpp

---
+++ pow.hpp
@@ -0,0 +1,52 @@
+#ifndef __POW_HPP__
+#define __POW_HPP__
+
+#include "base.hpp"
+#include <string>
+#include <iostream>
+#include <math.h>
+
+class Pow : public Base {
+        private:
+                Base* leftChild = NULL;
+                Base* rightChild = NULL;
+        public:
+                Pow(Base* left, Base* right) {
+                        leftChild = left;
+                        rightChild = right;
+                }
+                virtual double evaluate() {
+                        return pow(leftChild->evaluate(), rightChild->evaluate()); 
+                }
+                virtual std::string stringify() {
+                        std::stringstream out;
+                        out << leftChild->evaluate() << " ^ " << rightChild->evaluate();
+                        return out.str();
+                }
+		virtual int number_of_children() {
+           		 return 2;
+        	}
+        	virtual Base* get_child(int i) {
+            		if(i == 0) {
+				return leftChild;
+            		}
+			else if(i == 1) {
+				return rightChild;
+			}
+            		else return NULL;
+        	}
+		virtual void accept(Visitor* visitor, int index) {
+                          if (index == 0) {
+                                  visitor->visit_pow_begin(this);
+                          }
+                          else if (index == 1) {
+                                  visitor->visit_pow_middle(this);
+                          }
+                          else if (index == 2) {
+                                  visitor->visit_pow_end(this);
+                          }
+                 }
+
+};
+
+#endif //__POW_HPP__
