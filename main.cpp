3fccbd9c39b1a77cb7f0e2b0b38558eddb9ed52f
rohanbehera03 <rohanbehera03@gmail.com>
2021-05-25 10:10:54 -0700

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -21,22 +21,24 @@ std::string PrintLaTeX(Base* ptr){
 }
 
 int main(){
-	Base* val1 = new Op(1);
-	Base* val2 = new Op(2);
-	Base* val3 = new Op(3);
+    Base* val1 = new Op(1);
+    Base* val2 = new Op(2);
+    Base* val3 = new Op(3);
 
-	Subtract* s1 = new Sub(val2,val1);
-	Muliply* m1 = new Mult(val3,val2);
-	Divide* d1 = new Div(m1,s1);
 
-	Power* p1 = new Pow(val2, val3);
-	Addition* a1 = new Add(val2, val3);
-	Subtract* s2 = new Sub(p1,a1);
-	
-	Addition* add = new Add(d1,s2);
-	
-	std::cout << add->stringify() << "\n";
-	//std::cout << PrintMathML(add) << "\n";
+    Sub* s1 = new Sub(val2, val1);
+    Mult* m1 = new Mult(val3, val2);
+    Div* d1 = new Div(m1, s1);
+
+    Pow* p1 = new Pow(val2, val3);
+    Add* a1 = new Add(val2, val3);
+    Sub* s2 = new Sub(p1,a1);
+
+    Add* add = new Add(d1, s2);
+
+    std::cout << add->stringify() << " => \n LaTeX: ";
+
+    std::cout << PrintLaTeX(add) << "\n";
 
 	return 0;
 }
